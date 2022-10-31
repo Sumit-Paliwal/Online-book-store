6a58853f6d49adcf637b45ea5bb1784eebd7ef2a
Sumit-Paliwal <paliwalsumit13@gmail.com>
2022-10-31 20:08:57 +0530

    uri changed

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URL, {
+    const conn = await mongoose.connect("mongodb+srv://Sumit-Paliuwal:Sumit@987@cluster0.opwkmle.mongodb.net/?retryWrites=true&w=majority", {
       useUnifiedTopology: true,
       useNewUrlParser: true,
       useCreateIndex: true
