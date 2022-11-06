2da8d6c524368ac674e37e6ac794af884e634dbe
Sumit-Paliwal <110026385+Sumit-Paliwal@users.noreply.github.com>
2022-11-07 01:00:43 +0530

    Update generateToken.js

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -1,8 +1,8 @@
 import jwt from 'jsonwebtoken'
 
 const generateToken = (id) => {
-  return jwt.sign({id},process.env.JWT_SECRET, {
-    expiresIn: '10d'
+  return jwt.sign({id},"A9MyJ934rfZnSRySYM0Unm7XcSZsWndsXETORmJY5e3jhqE2J7VDTJ2eQcQIzE1bG+zfLkULrfwgNvgMwMLWSt5/nF8cYEd/fvDfrQ5Vf/cQvojKThC7DG6oHJjMBvjM3yjqRrQWgK+ul1vE/3xV1nq8ftUehUyYQfCxdGUg3jGcaI2G7oXe5ZpgCn7YauQ02VwvAoU7TTtW3iBVcDlSoLC/39lgJa0288/tfc+td43Nvz/cnqvOeJwc/vsept6mCKJsWWQJmH6LA4aWwQqu5Hs3ddb//R76oLjIpxKo5suiFTJrZyauvKr4yHZIxiHj4VOIAaTsy4pEOGcaHbEfgQ==", {
+    expiresIn: '90d'
   })
 }
 
