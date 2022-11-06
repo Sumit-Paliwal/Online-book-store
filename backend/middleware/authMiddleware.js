6f9d52d9a5fdb16a92e25fcdc8edba962d39a4bc
Sumit-Paliwal <110026385+Sumit-Paliwal@users.noreply.github.com>
2022-11-07 01:01:30 +0530

    Update authMiddleware.js

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -9,7 +9,7 @@ const safe = asyncHandler(async (req,res,next) => {
   if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')){
     try {
       token = req.headers.authorization.split(' ')[1]
-      const decoded = jwt.verify(token,process.env.JWT_SECRET)
+      const decoded = jwt.verify(token,"A9MyJ934rfZnSRySYM0Unm7XcSZsWndsXETORmJY5e3jhqE2J7VDTJ2eQcQIzE1bG+zfLkULrfwgNvgMwMLWSt5/nF8cYEd/fvDfrQ5Vf/cQvojKThC7DG6oHJjMBvjM3yjqRrQWgK+ul1vE/3xV1nq8ftUehUyYQfCxdGUg3jGcaI2G7oXe5ZpgCn7YauQ02VwvAoU7TTtW3iBVcDlSoLC/39lgJa0288/tfc+td43Nvz/cnqvOeJwc/vsept6mCKJsWWQJmH6LA4aWwQqu5Hs3ddb//R76oLjIpxKo5suiFTJrZyauvKr4yHZIxiHj4VOIAaTsy4pEOGcaHbEfgQ==")
       req.user = await User.findById(decoded.id).select('-password')
       next();
     }catch(err) {
